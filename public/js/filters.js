1796572056a5cc9533b633942ece32b7ac341ad9
teamdstn <teamdstn.dustin@gmail.com>
2013-05-21 22:31:24 +0900

    Update filters.js

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,25 +1,20 @@
 'use strict';
 
-/* ===================================
- filters.js
- - DateFilter
- =================================== */
-
 // DateFilter
 angular.module("vline.ui.DateFilter", [])
   .filter("dateFilter", ["$filter", function ($filter) {
-    var t = 864e5
-      , n = 6e4
-      , r = new Date
-      , i = r.getTimezoneOffset() * n
-      , s = r.getTime()
-      , o = s - (s - i) % t
-      ;//------
+  var _aDay     = 864e5
+    , _aMin     = 6e4
+    , _date     = new Date
+    , _timezone = _date.getTimezoneOffset() * _aMin
+    , _time     = _date.getTime()
+    , o = _time - (_time - _timezone) % _aDay
+    ;//------
 
-    return function (t) {
-      var n;
-      return t < o ? n = "short" : n = "shortTime", $filter("date")(t, n)
-    }
-  }]);
+  return function (t) {
+    var n;
+    return t < o ? n = "short" : n = "shortTime", $filter("date")(t, n)
+  }
+}]);
 
 
